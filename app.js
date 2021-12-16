e5cd64009fa7406aafa4a8e7670a49c51c4c128c
Sergio Almeciga <sergioalme24@hotmail.com>
2021-12-16 00:11:18 -0500

    se cambia la linea 18 del app.js

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ let nuevatarea=
 */
 //console.log(listartareas)
 //console.log(guardadato(datos_json,nuevatarea))
-//console.log(borrartarea(datos_json,36))
+console.log(borrartarea(datos_json,36))
 console.log(filtrartarea(datos_json,"En progreso"))
